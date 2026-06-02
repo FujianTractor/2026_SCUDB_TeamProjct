67e78623afcd2f846ae52349d34f3c7e84c62f81
fanqieshadingyv <983945595@qq.com>
2026-06-02 15:28:41 +0800

    Add methods to manage selected count in EduTeachingClassMapper

--- edu-admin/backend/src/main/java/com/scu/eduadmin/mapper/EduTeachingClassMapper.java
+++ edu-admin/backend/src/main/java/com/scu/eduadmin/mapper/EduTeachingClassMapper.java
@@ -2,5 +2,17 @@ package com.scu.eduadmin.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.scu.eduadmin.entity.EduTeachingClass;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
-public interface EduTeachingClassMapper extends BaseMapper<EduTeachingClass> {}
+public interface EduTeachingClassMapper extends BaseMapper<EduTeachingClass> {
+
+    @Select("SELECT selected_count FROM edu_teaching_class WHERE id = #{id}")
+    Integer getSelectedCountById(Long id);
+
+    @Update("UPDATE edu_teaching_class SET selected_count = selected_count + 1 " +
+            "WHERE id = #{teachingClassId} AND selected_count < #{capacity}")
+    int incrementSelectedCountIfUnderCapacity(@Param("teachingClassId") Long teachingClassId, 
+                                             @Param("capacity") Integer capacity);
+}
