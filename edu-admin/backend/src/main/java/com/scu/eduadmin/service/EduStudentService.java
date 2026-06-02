a217f12b82ece29d9cbec97fa90a6e40235b048a
fanqieshadingyv <983945595@qq.com>
2026-06-02 13:53:25 +0800

    Add pagination method to EduStudentService interface

--- edu-admin/backend/src/main/java/com/scu/eduadmin/service/EduStudentService.java
+++ edu-admin/backend/src/main/java/com/scu/eduadmin/service/EduStudentService.java
@@ -1,6 +1,11 @@
 package com.scu.eduadmin.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.scu.eduadmin.entity.EduStudent;
+import com.scu.eduadmin.vo.StudentVO;
 
-public interface EduStudentService extends IService<EduStudent> {}
+public interface EduStudentService extends IService<EduStudent> {
+
+    Page<StudentVO> selectStudentPage(long pageNum, long pageSize, Long classId, String keyword);
+}
