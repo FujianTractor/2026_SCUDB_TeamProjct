ac6b559a5b9f862131385ba82f8c260af57b3f48
fanqieshadingyv <983945595@qq.com>
2026-06-10 14:18:12 +0800

    Add search functionality to selectionId field

--- edu-admin/frontend/src/views/admin/GradeManage.vue
+++ edu-admin/frontend/src/views/admin/GradeManage.vue
@@ -1,9 +1,18 @@
-<template><CrudPage title="成绩管理" endpoint="/grades" :fields="fields" /></template>
+<template>
+  <CrudPage title="成绩管理" endpoint="/grades" :fields="fields" />
+</template>
+
 <script setup>
+import { ref } from 'vue'
 import CrudPage from '../../components/CrudPage.vue'
+
+import request from '@/utils/request' 
+
 const fields = [
   { prop: 'id', label: 'ID', edit: false },
-  { prop: 'selectionId', label: '选课ID', type: 'number' },
+  
+  { prop: 'selectionId', label: '选课ID', type: 'number', search: true }, 
+  
   { prop: 'score', label: '成绩', type: 'number' },
   { prop: 'gradePoint', label: '绩点', type: 'number' },
   { prop: 'gradeStatus', label: '状态', default: 'submitted' }
